52c976b8789f9ba5c79785fd122fe55a155257fb
mgamboa <gamboa.martin@gmail.com>
2023-01-30 12:15:51 -0600

    v0.1.0 Se inicializa paquete importador

---
+++ tests/controllers/controlador_adm_session_Test.php
@@ -0,0 +1,50 @@
+<?php
+namespace gamboamartin\importador\tests;
+
+
+use gamboamartin\errores\errores;
+use gamboamartin\importador\controllers\controlador_adm_session;
+use gamboamartin\test\test;
+
+use stdClass;
+
+
+class controlador_adm_session_Test extends test {
+    public errores $errores;
+    private stdClass $paths_conf;
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->errores = new errores();
+        $this->paths_conf = new stdClass();
+        $this->paths_conf->generales = '/var/www/html/acl/config/generales.php';
+        $this->paths_conf->database = '/var/www/html/acl/config/database.php';
+        $this->paths_conf->views = '/var/www/html/acl/config/views.php';
+    }
+
+    public function test_denegado(): void
+    {
+        errores::$error = false;
+
+        $_GET['seccion'] = 'adm_menu';
+        $_GET['accion'] = 'lista';
+        $_GET['registro_id'] = 1;
+        $_SESSION['grupo_id'] = 2;
+        $_SESSION['usuario_id'] = 2;
+        $_GET['session_id'] = '1';
+
+        
+
+        $controler = new controlador_adm_session(link: $this->link, paths_conf: $this->paths_conf);
+        //$controler = new liberator($controler);
+
+        $resultado = $controler->denegado(header: false);
+        $this->assertIsArray($resultado);
+        $this->assertNotTrue(errores::$error);
+        errores::$error = false;
+
+    }
+
+
+}
+
