8e6ef00f4a6ab015a4e4cb0a8636351f8981c4ed
mgamboa <gamboa.martin@gmail.com>
2023-01-30 16:33:04 -0600

    v0.5.0 Se integra imp database

---
+++ orm/imp_database.php
@@ -0,0 +1,30 @@
+<?php
+namespace gamboamartin\importador\models;
+use base\orm\_modelo_parent;
+use PDO;
+
+class imp_database extends _modelo_parent{
+
+    public function __construct(PDO $link){
+        $tabla = 'imp_database';
+        $columnas = array($tabla=>false,'imp_server'=>$tabla);
+        $campos_obligatorios[] = 'descripcion';
+        $campos_obligatorios[] = 'descripcion_select';
+        $campos_obligatorios[] = 'imp_server_id';
+        $campos_obligatorios[] = 'user';
+        $campos_obligatorios[] = 'password';
+        $campos_obligatorios[] = 'tipo';
+
+
+        $columnas_extra = array();
+        $tipo_campos = array();
+
+
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, columnas_extra: $columnas_extra, tipo_campos: $tipo_campos);
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+
+
+}
