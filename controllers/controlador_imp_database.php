f521d3f6925f34036c9c7b6734af562e28838ad9
mgamboa <gamboa.martin@gmail.com>
2023-01-31 10:15:11 -0600

    v0.10.0 Se integra origen a db

--- controllers/controlador_imp_database.php
+++ controllers/controlador_imp_database.php
@@ -15,6 +15,7 @@ use gamboamartin\importador\models\imp_database;
 use gamboamartin\system\_ctl_parent_sin_codigo;
 use gamboamartin\system\links_menu;
 use gamboamartin\template_1\html;
+use html\adm_seccion_html;
 use PDO;
 use stdClass;
 
@@ -23,6 +24,8 @@ class controlador_imp_database extends _ctl_parent_sin_codigo {
 
     public stdClass|array $imp_database = array();
     private imp_database_html $html_local;
+
+    public string $link_imp_origen_alta_bd = '';
 
 
     public function __construct(PDO $link, html $html = new html(), stdClass $paths_conf = new stdClass()){
@@ -64,6 +67,16 @@ class controlador_imp_database extends _ctl_parent_sin_codigo {
             }
             $this->imp_database = $imp_database;
         }
+
+        $link_imp_origen_alta_bd = $this->obj_link->link_alta_bd(link: $link, seccion: 'imp_origen');
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al obtener link',data:  $link_imp_origen_alta_bd);
+            print_r($error);
+            exit;
+        }
+        $this->link_imp_origen_alta_bd = $link_imp_origen_alta_bd;
+
+
 
 
     }
@@ -112,6 +125,31 @@ class controlador_imp_database extends _ctl_parent_sin_codigo {
         }
 
         return $campos_view;
+    }
+
+    protected function inputs_children(stdClass $registro): array|stdClass{
+        $select_imp_database_id = (new imp_database_html(html: $this->html_base))->select_imp_database_id(
+            cols:12,con_registros: true,id_selected:  $registro->imp_database_id,link:  $this->link, disabled: true);
+
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al obtener select_imp_database_id',data:  $select_imp_database_id);
+        }
+
+        $select_adm_seccion_id = (new adm_seccion_html(html: $this->html_base))->select_adm_seccion_id(
+            cols:12,con_registros: true,id_selected:  -1,link:  $this->link, disabled: false);
+
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al obtener select_imp_database_id',data:  $select_adm_seccion_id);
+        }
+
+
+
+
+        $this->inputs = new stdClass();
+        $this->inputs->imp_database_id = $select_imp_database_id;
+        $this->inputs->adm_seccion_id = $select_adm_seccion_id;
+
+        return $this->inputs;
     }
 
 
@@ -171,6 +209,29 @@ class controlador_imp_database extends _ctl_parent_sin_codigo {
 
 
         return $r_modifica;
+    }
+
+    public function origenes(bool $header = true, bool $ws = false): array|stdClass|string
+    {
+
+        $data_view = new stdClass();
+        $data_view->names = array('Id','Origen', 'DB','IP','Seccion','Acciones');
+        $data_view->keys_data = array('imp_origen_id','imp_origen_descripcion','imp_database_descripcion',
+            'imp_server_ip','adm_seccion_descripcion');
+        $data_view->key_actions = 'acciones';
+        $data_view->namespace_model = 'gamboamartin\\importador\\models';
+        $data_view->name_model_children = 'imp_origen';
+
+
+        $contenido_table = $this->contenido_children(data_view: $data_view, next_accion: __FUNCTION__, not_actions: $this->not_actions);
+        if(errores::$error){
+            return $this->retorno_error(
+                mensaje: 'Error al obtener tbody',data:  $contenido_table, header: $header,ws:  $ws);
+        }
+
+
+        return $contenido_table;
+
     }
 
 
--- templates/directivas/imp_database_html.php
+++ templates/directivas/imp_database_html.php
@@ -9,12 +9,12 @@ use PDO;
 
 class imp_database_html extends html_controler {
 
-    public function select_imp_server_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_imp_database_id(int $cols, bool $con_registros, int $id_selected, PDO $link, bool $disabled = false): array|string
     {
         $modelo = new imp_database(link: $link);
 
-        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo,label: 'DB',required: true);
+        $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
+            modelo: $modelo, disabled: $disabled, label: 'DB', required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
