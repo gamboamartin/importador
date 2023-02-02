6aacfe09911c4579515a8344cceea6f9f8de4065
mgamboa <gamboa.martin@gmail.com>
2023-02-02 13:00:32 -0600

    v0.35.0 Se integra adm de campos desde tipo dato

--- controllers/controlador_adm_tipo_dato.php
+++ controllers/controlador_adm_tipo_dato.php
@@ -8,18 +8,24 @@ use gamboamartin\importador\models\adm_tipo_dato;
 use gamboamartin\system\_ctl_base;
 use gamboamartin\system\links_menu;
 use gamboamartin\template_1\html;
+use html\adm_seccion_html;
 use PDO;
 use stdClass;
 
 class controlador_adm_tipo_dato extends _ctl_base {
 
+    private adm_tipo_dato_html $html_local;
+    public string $link_adm_campo_alta_bd = '';
 
     public function __construct(PDO $link, html $html = new html(), array $datatables_custom_cols = array(),
                                 array $datatables_custom_cols_omite = array(), stdClass $paths_conf = new stdClass()){
         $modelo = new adm_tipo_dato(link: $link);
 
         $html_ = new adm_tipo_dato_html(html: $html);
+        $this->html_local = $html_;
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
+
+
 
         $datatables = new stdClass();
         $datatables->columns = array();
@@ -36,13 +42,19 @@ class controlador_adm_tipo_dato extends _ctl_base {
 
         $this->titulo_lista = 'Tipos de datos';
 
+        $link_adm_campo_alta_bd = $this->obj_link->link_alta_bd(link: $link, seccion: 'adm_campo');
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al obtener link',data:  $link_adm_campo_alta_bd);
+            print_r($error);
+            exit;
+        }
+        $this->link_adm_campo_alta_bd = $link_adm_campo_alta_bd;
+
 
         $this->lista_get_data = true;
 
     }
-
-
-    public function alta(bool $header, bool $ws = false): array|string
+    final public function alta(bool $header, bool $ws = false): array|string
     {
 
         $r_alta = $this->init_alta();
@@ -65,7 +77,29 @@ class controlador_adm_tipo_dato extends _ctl_base {
         return $r_alta;
     }
 
-    protected function campos_view(): array
+    final public function campos(bool $header = true, bool $ws = false): array|stdClass|string
+    {
+
+        $data_view = new stdClass();
+        $data_view->names = array('Id','Campo', 'Tipo Dato','Seccion','Acciones');
+        $data_view->keys_data = array('adm_campo_id','adm_campo_descripcion','adm_tipo_dato_descripcion','adm_seccion_descripcion');
+        $data_view->key_actions = 'acciones';
+        $data_view->namespace_model = 'gamboamartin\\administrador\\models';
+        $data_view->name_model_children = 'adm_campo';
+
+
+        $contenido_table = $this->contenido_children(data_view: $data_view, next_accion: __FUNCTION__, not_actions: $this->not_actions);
+        if(errores::$error){
+            return $this->retorno_error(
+                mensaje: 'Error al obtener tbody',data:  $contenido_table, header: $header,ws:  $ws);
+        }
+
+
+        return $contenido_table;
+
+    }
+
+    final protected function campos_view(): array
     {
         $keys = new stdClass();
         $keys->inputs = array('codigo','descripcion');
@@ -83,7 +117,7 @@ class controlador_adm_tipo_dato extends _ctl_base {
         return $campos_view;
     }
 
-    public function get_adm_tipo_dato(bool $header, bool $ws = true): array|stdClass
+    final public function get_adm_tipo_dato(bool $header, bool $ws = true): array|stdClass
     {
 
         $keys['adm_tipo_dato'] = array('id','descripcion','codigo');
@@ -96,6 +130,35 @@ class controlador_adm_tipo_dato extends _ctl_base {
 
         return $salida;
 
+    }
+
+    protected function inputs_children(stdClass $registro): array|stdClass{
+
+        $select_adm_tipo_dato_id = (new adm_tipo_dato_html(html: $this->html_base))->select_adm_tipo_dato_id(
+            cols:12,con_registros: true,id_selected:  $this->registro_id,link:  $this->link, disabled: true);
+
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al obtener select_adm_tipo_dato_id',data:  $select_adm_tipo_dato_id);
+        }
+
+        $select_adm_seccion_id = (new adm_seccion_html(html: $this->html_base))->select_adm_seccion_id(
+            cols:12,con_registros: true,id_selected:  -1,link:  $this->link, disabled: false);
+
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al obtener select_adm_seccion_id',data:  $select_adm_seccion_id);
+        }
+
+        $adm_campo_descripcion = $this->html_local->input_descripcion(cols: 12, row_upd: new stdClass(),value_vacio: false);
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al obtener adm_campo_descripcion',data:  $adm_campo_descripcion);
+        }
+
+        $this->inputs = new stdClass();
+        $this->inputs->adm_tipo_dato_id = $select_adm_tipo_dato_id;
+        $this->inputs->adm_seccion_id = $select_adm_seccion_id;
+        $this->inputs->adm_campo_descripcion = $adm_campo_descripcion;
+
+        return $this->inputs;
     }
 
 
