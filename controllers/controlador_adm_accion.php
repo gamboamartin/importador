5d2b9e0e0473a8c32e1775c048b4b0cbe06a2e7a
mgamboa <gamboa.martin@gmail.com>
2023-03-17 13:37:08 -0600

    v2.2.0 Se integra data seguridad

---
+++ controllers/controlador_adm_accion.php
@@ -0,0 +1,3 @@
+<?php
+namespace gamboamartin\importador\controllers;
+class controlador_adm_accion extends \gamboamartin\acl\controllers\controlador_adm_accion {}
---
+++ controllers/controlador_adm_grupo.php
@@ -0,0 +1,3 @@
+<?php
+namespace gamboamartin\importador\controllers;
+class controlador_adm_grupo extends \gamboamartin\acl\controllers\controlador_adm_grupo {}
