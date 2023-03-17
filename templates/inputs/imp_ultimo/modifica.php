302cb4750d0852ba8631ce661e360c8062d71641
mgamboa <gamboa.martin@gmail.com>
2023-03-17 13:53:11 -0600

    v2.4.0 Se ajsuytan vierws importador

--- templates/inputs/imp_ultimo/modifica.php
+++ templates/inputs/imp_ultimo/modifica.php
@@ -6,10 +6,5 @@
 
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
 
-<div class="col-row-12">
-    <?php foreach ($controlador->buttons as $button){ ?>
-        <?php echo $button; ?>
-    <?php }?>
-</div>
 
 
